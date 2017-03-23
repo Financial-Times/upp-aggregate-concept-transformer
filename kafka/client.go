1cb7194269ba4a1d73e6d45613e444848b0659cd
Scott Newton <scott.newton@ft.com>
2017-03-23 16:37:23 +0000

    Switched from async to sync kafka messages

--- kafka/client.go
+++ kafka/client.go
@@ -2,48 +2,25 @@ package kafka
 
 import (
 	"github.com/Shopify/sarama"
-	log "github.com/Sirupsen/logrus"
 )
 
 type Client struct {
-	producer sarama.AsyncProducer
-	topic    string
+	Producer sarama.SyncProducer
+	Topic    string
 }
 
-func NewClient(zkAddresses []string, topic string) (Client, error) {
+func NewClient(kafkaAddress string, topic string) (Client, error) {
 	config := sarama.NewConfig()
 	config.Producer.Return.Successes = false
 	config.Producer.Return.Errors = true
 
-	producer, err := sarama.NewAsyncProducer(zkAddresses, config)
+	producer, err := sarama.NewSyncProducer([]string{kafkaAddress}, config)
 	if err != nil {
 		return Client{}, err
 	}
 
-	go func() {
-		for pm := range producer.Successes() {
-			log.Infof("Successfully produced message on topic %s", pm.Topic)
-		}
-	}()
-
-	go func() {
-		for err := range producer.Errors() {
-			log.Errorf("Error producing message: %s", err)
-		}
-	}()
-
 	return Client{
-		producer: producer,
-		topic:    topic,
+		Producer: producer,
+		Topic:    topic,
 	}, nil
-}
-
-func (c *Client) SendMessage(message string) {
-
-	m := &sarama.ProducerMessage{
-		Topic: c.topic,
-		Value: sarama.StringEncoder(message),
-	}
-
-	c.producer.Input() <- m
 }
